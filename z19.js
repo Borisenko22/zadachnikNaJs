3a6d18e6671660f4ddc0000bf037c7a8085bb833
Nickolas Borisenko <borisenkonc@gmail.com>
2024-01-10 22:31:13 +0200

    Задача 20 Done!

---
+++ z19.js
@@ -0,0 +1,18 @@
+// Задача 19: Даны десять вещественных чисел. 
+// Найти их сумму
+let a = [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ].reduce(function (previousValue, currentValue,) {
+    return previousValue + currentValue;
+  });
+  console.log(a);
+    
+
+    
+
+
+// let sum = 0; 
+// for(let i = 0; i<a.length; i++){
+//     sum = sum + a[i];
+// }
+// console.log(a.length)
+// console.log(sum);
+
